1c171774704331f34d764876ca4a2fdc71aa2902
hawkwzh <wzh_90@163.com>
2019-06-21 15:57:21 +0800

    hello world

---
+++ hello_world/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var a = "sss"
+var b float32 = 11
+var c = 1.2
+
+func main() {
+	fmt.Println("Hello World.")
+	fmt.Println("Hello World.")
+	fmt.Println("Hello World.")
+	fmt.Println("c=" + a)
+}
